3245d7cf16e34faa03c682ab2efaff99e1193fb4
rkumar <rkredux@gmail.com>
2020-06-14 21:13:31 -0400

    Consumer updates

--- src/main/java/com/github/rkredux/ConsumerDemo.java
+++ src/main/java/com/github/rkredux/ConsumerDemo.java
@@ -1,4 +1,51 @@
 package com.github.rkredux;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 public class ConsumerDemo {
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "sixth_group";
+        String topic = "third_topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> kafkaConsumer =
+                new KafkaConsumer<>(properties);
+
+        kafkaConsumer.subscribe(Collections.singleton(topic));
+        //    poll for data
+        while (true) {
+            ConsumerRecords<String, String> records =
+                    kafkaConsumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Received new record \n" +
+                        "Key:" + record.key() + "\n" +
+                        "Value:" + record.value() + "\n" +
+                        "Partition:" + record.partition() + "\n" +
+                        "Offset:" + record.offset() + "\n"
+                );
+
+            }
+
+        }
+
+    }
 }
