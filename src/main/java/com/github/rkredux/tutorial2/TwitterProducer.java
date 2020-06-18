9b41272df7cce87bc77b5f033243d303dbc9e0eb
rkumar <rkredux@gmail.com>
2020-06-18 09:33:22 -0400

    Creating Twitter Client

--- src/main/java/com/github/rkredux/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/rkredux/tutorial2/TwitterProducer.java
@@ -1,61 +1,79 @@
 package com.github.rkredux.tutorial2;
 
 import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
-    //create all the sub classes and methods off of it
-    //use the run to trigger an execution from main function
 
-    //function named same as the parent class does not need a return type
-    //also called as a constructor function
-    //a constructor function runs when you use the keyword new to create
-    //an object of instance class
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+
     public TwitterProducer(){
 
     }
 
-
     public static void main(String[] args) {
-        TwitterProducer twitterProducer = new TwitterProducer();
-        twitterProducer.run();
+        new TwitterProducer().run();
     }
 
     public void run(){
-        //  create Twitter client
-        //  get the tweets from Twitter
-        //  Loop over the tweets and ship them to a topic in Kafka
+
+        logger.info("Setting up the client");
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+        Client client = createTwitterClient(msgQueue);
+        client.connect();
+        //create kafka producer
+        //loop to send tweets to Kafka
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+                client.stop();
+            }
+            if (msg != null){
+                logger.info(msg);
+            }
+        }
+        logger.info("End of application");
     }
 
-    public void createTwitterClient(){
-        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
-        //BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>(1000);
-
+    public Client createTwitterClient(BlockingQueue<String> queue){
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
         Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
         // Optional: set up some followings and track terms
-        ArrayList<Long> followings = Lists.newArrayList(1234L, 566788L);
-        ArrayList<String> terms = Lists.newArrayList("twitter", "api");
-        hosebirdEndpoint.followings(followings);
+        ArrayList<String> terms = Lists.newArrayList("kafka");
         hosebirdEndpoint.trackTerms(terms);
-
-        // These secrets should be read from a config file
+        //TO DO create a secrets passing mechanism so we don't commit in the code
         Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
 
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client")
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(queue));
 
-
+        Client hosebirdClient = builder.build();
+        //Attempts to establish a connection.
+        return hosebirdClient;
     }
 
 
