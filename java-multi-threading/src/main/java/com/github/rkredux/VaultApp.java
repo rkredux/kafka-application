aae3e2102f15ac86c7e1577993f5aad2df357da7
rkumar <rkredux@gmail.com>
2020-07-11 22:05:27 -0400

    Adding System exit when password guessed correctly

--- java-multi-threading/src/main/java/com/github/rkredux/VaultApp.java
+++ java-multi-threading/src/main/java/com/github/rkredux/VaultApp.java
@@ -1,15 +1,31 @@
 package com.github.rkredux;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class VaultApp {
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int password = rand.nextInt(1000);
+        int password = rand.nextInt(100);
         Vault vault = new Vault(password);
-        Robber robber = new Robber(vault, "RobberOne");
-        robber.start();
+        //start four robber threads
+        Robber robberOne = new Robber(vault, "RobberOne");
+        Robber robberTwo = new Robber(vault, "RobberTwo");
+        Robber robberThree = new Robber(vault, "RobberThree");
+        Robber robberFour = new Robber(vault, "RobberFour");
+        //add them to the list
+        List<Robber> listOfRobbers = new ArrayList<Robber>();
+        listOfRobbers.add(robberOne);
+        listOfRobbers.add(robberTwo);
+        listOfRobbers.add(robberThree);
+        listOfRobbers.add(robberFour);
+        //start a robber thread for each robber
+        for (Robber robber: listOfRobbers){
+            robber.start();
+        }
     }
 
     private static class Vault {
@@ -31,6 +47,7 @@ public class VaultApp {
     private static class Robber extends Thread{
         private final Vault vault;
         private final String name;
+
         public Robber(Vault vault, String name){
             this.vault = vault;
             this.name = name;
@@ -40,20 +57,21 @@ public class VaultApp {
             super.run();
             int counter = 1;
             Thread.currentThread().setName(name);
-            boolean hasPasswordNotBeenGuessed = true;
-            while (hasPasswordNotBeenGuessed){
+            boolean passWordGuessed = false;
+            while (!passWordGuessed){
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 Random rand = new Random();
-                int guess = rand.nextInt(1000);
+                int guess = rand.nextInt(100);
                 if (vault.isACorrectPasswordGuess(guess, vault.password)){
                     System.out.println("Password was guessed correctly by Robber: " + Thread.currentThread().getName() + " In " + counter + " attempts");
-                    return;
+                    passWordGuessed = true;
+                    System.exit(0);
                 };
-                System.out.println("Password not guessed correctly in " + counter + " attempt, attempting again");
+                //System.out.println("Password not guessed correctly in " + counter + " attempt, attempting again");
                 counter = counter + 1;
             }
         }
