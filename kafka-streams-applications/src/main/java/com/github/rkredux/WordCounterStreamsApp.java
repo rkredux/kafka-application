28c8604adc474e508f0957dfe9cfcf6aa6da47b7
rkumar <rkredux@gmail.com>
2020-07-17 08:41:29 -0400

    Adding Lambda Expressions For Filter Kafka Stream

--- kafka-streams-applications/src/main/java/com/github/rkredux/WordCounterStreamsApp.java
+++ kafka-streams-applications/src/main/java/com/github/rkredux/WordCounterStreamsApp.java
@@ -2,7 +2,10 @@ package com.github.rkredux;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.Properties;
 
@@ -16,5 +19,16 @@ public class WordCounterStreamsApp {
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, String.valueOf(Serdes.String().getClass()));
         properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, String.valueOf(Serdes.String().getClass()));
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, Long> stream = streamsBuilder.stream("word-stream");
+        final KStream<String, Long> filter = stream.filter((key, value) -> value > 0);
+
+        //    build the topology using Streams Builder
+    //    source processor
+    //    sink processor
+    //    node
+
     }
 }
