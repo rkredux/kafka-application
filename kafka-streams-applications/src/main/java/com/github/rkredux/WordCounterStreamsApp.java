2dc3a859a269e27f77680c6099bcbe00d1afa830
rkumar <rkredux@gmail.com>
2020-07-17 21:33:54 -0400

    Ktable sink to kafka topic

--- kafka-streams-applications/src/main/java/com/github/rkredux/WordCounterStreamsApp.java
+++ kafka-streams-applications/src/main/java/com/github/rkredux/WordCounterStreamsApp.java
@@ -2,35 +2,35 @@ package com.github.rkredux;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.util.Arrays;
 import java.util.Properties;
 
 public class WordCounterStreamsApp {
-    public static void main(String[] args) {
+    public static void main(final String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        String bootstrapServers = "127.0.0.1:9092";
-        Properties properties = new Properties();
-        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
-        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "streams-word-counter-app" );
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, String.valueOf(Serdes.String().getClass()));
-        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, String.valueOf(Serdes.String().getClass()));
-
-        StreamsBuilder streamsBuilder = new StreamsBuilder();
-        KStream<String, Long> wordCountInputStream = streamsBuilder.stream("word-stream");
-        //final KStream<String, String> wordCountInputStream = streamBuilder.stream("words-stream");
-        KTable<String, Long> wordCountOutputTable = wordCountInputStream
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> textLines = builder.stream("TextLinesTopic");
+        KTable<String, Long> wordCounts = textLines
                 .mapValues((value) -> value.toLowerCase())
-                .flatMapValues((value) -> Arrays.asList(value.split(" ")))
-                .selectKey((key, value) -> value)
+                .flatMapValues(lowerCasedValue -> Arrays.asList(lowerCasedValue.split(" ")))
+                .selectKey((key,value) -> value)
                 .groupByKey()
-                .count();
+                .count(Named.as("Counts"));
+        wordCounts.toStream().to("WordsWithCountsTopic", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
     }
 }
