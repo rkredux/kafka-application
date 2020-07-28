021deb4f666e45528f09325fcafced7af4e6e77d
rkumar <rkredux@gmail.com>
2020-07-28 06:48:03 -0400

    Aggregator streams set up

--- kafka-streams-applications/src/main/java/com/github/rkredux/BankBalanceApp.java
+++ kafka-streams-applications/src/main/java/com/github/rkredux/BankBalanceApp.java
@@ -1,21 +1,50 @@
 package com.github.rkredux;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.*;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
 import java.util.Date;
 import java.util.Properties;
 import java.util.UUID;
 import java.sql.Timestamp;
 
 public class BankBalanceApp {
+
     public static void main(String[] args) {
         String bootstrapServers = "127.0.0.1:9092";
-        String topicName = "customer-transactions-topic";
+        String customerTransactionsTopic = "customer-transactions-topic";
+        String aggregatedBankBalanceTopic = "customer-balance-topic";
         Integer countOfTransactions = 10;
-        BankTransactionsProducer bankTransactionsProducer = new BankTransactionsProducer(bootstrapServers, topicName, countOfTransactions);
+
+        BankTransactionsProducer bankTransactionsProducer = new BankTransactionsProducer(
+                bootstrapServers,
+                customerTransactionsTopic,
+                countOfTransactions
+        );
+        System.out.println("Starting generating bank transactions");
         bankTransactionsProducer.start();
+
+        BankBalanceAggregator bankBalanceAggregator = new BankBalanceAggregator(
+                customerTransactionsTopic,
+                aggregatedBankBalanceTopic
+        );
+        System.out.println("Starting bank balance aggregation stream processing");
+        bankBalanceAggregator.start();
     }
 
     private static class BankTransactionsProducer extends Thread{
@@ -94,5 +123,75 @@ public class BankBalanceApp {
         }
     }
 
+
+    private static class BankBalanceAggregator extends Thread {
+
+        private final String aggregatedBankBalanceTopic;
+        private final String customerTransactionsTopic;
+
+        public BankBalanceAggregator(String customerTransactionsTopic, String aggregatedBankBalanceTopic) {
+            this.aggregatedBankBalanceTopic = aggregatedBankBalanceTopic;
+            this.customerTransactionsTopic = customerTransactionsTopic; 
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            //streams config
+            Properties props = new Properties();
+            props.put(StreamsConfig.APPLICATION_ID_CONFIG, "bank-balance-app");
+            props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+            props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+            props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+            props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+            //JSON Serde set up
+            final Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+            final Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+            final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+
+            //building streams topology
+            StreamsBuilder builder = new StreamsBuilder();
+
+            KStream<String, JsonNode> customerTransactions = builder.stream(customerTransactionsTopic,
+                    Consumed.with(Serdes.String(), jsonSerde));
+
+            //creating initial bank balance object
+            ObjectNode initialBalance = JsonNodeFactory.instance.objectNode();
+            initialBalance.put("count", 0);
+            initialBalance.put("balance", 0);
+            initialBalance.put("time", Instant.ofEpochMilli(0L).toString());
+
+            KTable<String, JsonNode> bankBalance = customerTransactions
+                    .groupByKey()
+                    .aggregate(() -> initialBalance,
+                            (key,transaction,balance) -> newBalance(transaction,balance),
+                            Materialized.<String, JsonNode, KeyValueStore<Bytes, byte[]>>as("bank-balance-agg")
+                            .withKeySerde(Serdes.String())
+                            .withValueSerde(jsonSerde)
+                    );
+
+            bankBalance.toStream().to(aggregatedBankBalanceTopic, Produced.with(Serdes.String(), jsonSerde));
+
+            KafkaStreams streamsApp = new KafkaStreams(builder.build(), props);
+            //streamsApp.cleanUp();
+            streamsApp.start();
+            System.out.println(streamsApp.toString());
+            Runtime.getRuntime().addShutdownHook(new Thread(streamsApp::close));
+        }
+
+        //function to aggregate the records
+        private static JsonNode newBalance(JsonNode transaction, JsonNode balance) {
+            ObjectNode newBalance = JsonNodeFactory.instance.objectNode();
+            newBalance.put("count", balance.get("count").asInt() + 1);
+            newBalance.put("balance", balance.get("balance").asInt() + transaction.get("amount").asInt());
+            Long balanceEpoch = Instant.parse(balance.get("time").asText()).toEpochMilli();
+            Long transactionEpoch = Instant.parse(transaction.get("time").asText()).toEpochMilli();
+            Instant newBalanceInstant = Instant.ofEpochMilli(Math.max(balanceEpoch, transactionEpoch));
+            newBalance.put("time", newBalanceInstant.toString());
+            return newBalance;
+        }
+
+    }
 
 }
